3ec629b4ec427fda7b5fae088d68b5e8393425a5
Davy2005 <davyli.contact@gmail.com>
2026-03-15 17:44:55 +0100

    Delete 2_contraintes.sql

--- 2_contraintes.sql
+++
@@ -1,9 +0,0 @@
--- Étape III.B : Contraintes de validation supplémentaires
-
-ALTER TABLE OFFRE_ABONNEMENT ADD CONSTRAINT chk_prix_positif CHECK (prix_mensuel >= 0);
-
-ALTER TABLE id_utilisateur ADD CONSTRAINT chk_email_format CHECK (email LIKE '%@%.%');
-
-ALTER TABLE PISTE ADD CONSTRAINT chk_duree_piste CHECK (duree_sec > 0);
-
-ALTER TABLE ABONNEMENT ADD CONSTRAINT chk_dates_abonnement CHECK (date_fin >= date_debut);
